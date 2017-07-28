bd7bcf0be76159c8bb1be077419db50403b64d1b
parsiansoft <mohsen.azadfallah@yahoo.com>
2017-07-28 11:38:43 +0430

    96-05-06

--- PersianMIS/CurrentState/UCShowCurrentStateWithChart.cs
+++ PersianMIS/CurrentState/UCShowCurrentStateWithChart.cs
@@ -18,9 +18,7 @@ namespace PersianMIS.CurrentState
 
         public UCShowCurrentStateWithChart()
         {
-
             InitializeComponent();
-         
             Theme theme = Theme.ReadCSSText(@"
                                             theme
                                             {
@@ -50,9 +48,6 @@ namespace PersianMIS.CurrentState
 
         }
 
-
-
-
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
@@ -74,56 +69,19 @@ namespace PersianMIS.CurrentState
             bar.DataSource = BLL.Cls_PublicOperations.Dt;
             this.MainChart.Series.Add(bar);
 
-            MyFormatProvider myFormatter = new MyFormatProvider();
-            myFormatter.Start = new DateTime(2013, 6, 5, 8, 0, 0);
-            bar.VerticalAxis.LabelFormatProvider = myFormatter;
+            //MyFormatProvider myFormatter = new MyFormatProvider();
+            //myFormatter.Start = new DateTime(2013, 6, 5, 8, 0, 0);
+            //bar.VerticalAxis.LabelFormatProvider = myFormatter;
 
 
-           
+
 
         }
         private void FillChartData()
         {
-           
+
         }
 
-        private void radCheckBox1_ToggleStateChanged(object sender, StateChangedEventArgs args)
-        {
-            
-        }
-
-        //private void FillChartData()
-        //    {
-        //        BLL.Cls_PublicOperations.Dt = BllStation.GetAllStationData("2017/07/19", "2017/07/26", "1");
-
-        //        //WaterfallSeries series = new WaterfallSeries("Value", "Summary", "Total", "Category");
-        //        //series.ShowLabels = true;
-        //        //series.DataSource = BLL.Cls_PublicOperations.Dt;
-
-
-        //        CartesianArea area = this.MainChart.GetArea();
-        //        area.ShowGrid = true;
-        //        LinearAxis verticalAxis = new LinearAxis();
-        //        verticalAxis.LabelFormat = "{0}°C";
-        //        verticalAxis.AxisType = AxisType.Second;
-        //        DateTimeContinuousAxis horizontalAxis = new DateTimeContinuousAxis();
-        //        horizontalAxis.LabelFormat = "{0:MMM}";
-        //        WeatherModel model = new WeatherModel();
-        //        RangeBarSeries barSeries = new RangeBarSeries("High", "Low", "Time");
-        //        barSeries.Name = "Temperature";
-        //        barSeries.HorizontalAxis = horizontalAxis; barSeries.VerticalAxis = verticalAxis;
-        //        this.radChartView1.Series.Add(barSeries);
-        //        this.radChartView1.DataSource = model.GetTemperatureData();
-        //        CartesianGridLineAnnotation line2011 = new CartesianGridLineAnnotation(horizontalAxis, new DateTime(2011, 1, 1));
-        //        line2011.Label = "2011"; line2011.DrawMode = AnnotationDrawMode.BelowSeries;
-        //        CartesianGridLineAnnotation line2012 = new CartesianGridLineAnnotation(horizontalAxis, new DateTime(2012, 1, 1));
-        //        line2012.Label = "2012";
-        //        line2012.DrawMode = AnnotationDrawMode.BelowSeries;
-        //        this.radChartView1.Area.Annotations.Add(line2011);
-        //        this.radChartView1.Area.Annotations.Add(line2012);
-        //        this.radChartView1.View.Palette = KnownPalette.Metro;
-        //        this.MainChart.Series.Add(series);
-        //    }
 
     }
 }
